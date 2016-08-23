90cd3164f065a9cf0586632d3025689e2ee0878a
Mayur <mayur.gupta@polestarllp.com>
2016-08-23 18:23:07 +0530

    changes in project

--- model/userauthentication.js
+++ model/userauthentication.js
@@ -1,12 +1,13 @@
+
 'use strict';
 
 var mysql = require('../lib/mysql').executeQuery;
-
+ var ip = require('ip');
 module.exports = {
 	login: function(userid, password, callback) {
 		var query = {
-			sql: 'call usp_validUser(?,?)',
-			values: [userid, password]
+			sql: 'call usp_validUser(?,?,?)',
+			values: [userid, password,ip.address()]
 		};
 		mysql(query, function(err, result) {
 			callback(err, result);
