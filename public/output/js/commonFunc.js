292c6b798800e42a2494f4c1ff1e3f8281a42757
Ashish Singla <ashish.singla@polestarllp.com>
2016-09-07 17:03:01 +0530

    add user client

--- public/output/js/commonFunc.js
+++ public/output/js/commonFunc.js
@@ -10,3 +10,20 @@ function messageBox(message) {
 	$("#ModalAddMesage").html(messageBox);
 	$("#activeBugMasterMessageModal").modal("show");
 };
+function confirmBox2(value,name,flag) { 
+	var msg="";
+	if(flag == 2) {
+	msg +='Are you sure you want to Activate?';
+	} else if(flag == 1){
+	msg +='Are you sure you want to Dectivate?';  
+	} else if(flag == 4){ 
+	msg +='Are you sure you want to delete this mapping?';  
+	} else if(flag == 5){ 
+	msg +='Access for fusion will be revoked for this user.Do you want to continue?';  
+	} else if(flag == 6) {
+		msg +='Mail will be send to for activation.Do you want to continue?';
+	}
+	var confirmBox ='<div class="modal fade" id="activeBugMasterModal" role="dialog"  data-backdrop="static" data-keyboard="false"><div class="modal-dialog" id="commonModalId"><div class="modal-content" id="activeBugMasterCnfrmMsgBox"> <div class="modal-header text-center" style="border-top:0px;"> <span>Message</span> </div><div class="modal-body text-center"> <span id="cMessage1">'+msg+'</span> </div><div class="modal-footer text-center" style="border-top:0px;"> <button id="activeBugMasterCloseMessage" type="reset" data-dismiss="modal" class="btn btn-default" style="background-color:#f05f40;color:white;padding-top: 4px; width:100px;height:30px;margin-top:10px;">Cancel</button> <button id="activeBugMasterActive" type="submit" data-dismiss="modal" class="btn btn-default" style="background-color:#f05f40;color:white;padding-top: 4px; width:100px;height:30px;margin-top:10px;" onclick='+name+'('+value+');>Ok</button> </div></div></div></div>';
+	$("#ModalAdd").html(confirmBox);
+	$("#activeBugMasterModal").modal("show");
+};
