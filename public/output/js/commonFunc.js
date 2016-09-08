b2877959e8757c1035539e2ca254ba0697d5be5a
Ashish Singla <ashish.singla@polestarllp.com>
2016-09-08 17:45:25 +0530

    wps

--- public/output/js/commonFunc.js
+++ public/output/js/commonFunc.js
@@ -54,6 +54,8 @@ function confirmBox3(value1,value2,flag) {
 		msg +='Do you want to create WBS?';
 	} else if(flag == 8) {
 		msg +='Do you want to create more WBS and Assignment?';
+	} else if(flag == 9) {
+		msg +='WBS Added Successfully.Do you want to create more WBS?';
 	}
 	var confirmBox ='<div class="modal fade" id="activeBugMasterModal" role="dialog" data-backdrop="static" data-keyboard="false"><div class="modal-dialog" id="commonModalId"><div class="modal-content" id="activeBugMasterCnfrmMsgBox"> <div class="modal-header text-center" style="border-top:0px;"> <span>Message</span> </div><div class="modal-body text-center"> <span id="cMessage1">'+msg+'</span> </div><div class="modal-footer text-center" style="border-top:0px;"> <button id="activeBugMasterCloseMessage" type="submit" data-dismiss="modal" class="btn btn-default" style="background-color:#f05f40;color:white;padding-top: 4px; width:100px;height:30px;margin-top:10px;" onclick='+value2+';>Cancel</button> <button id="activeBugMasterActive" type="submit" data-dismiss="modal" class="btn btn-default" style="background-color:#f05f40;color:white;padding-top: 4px; width:100px;height:30px;margin-top:10px;" onclick='+value1+';>Ok</button> </div></div></div></div>';
 	$("#ModalAdd").html(confirmBox);
