26c12920eb066637e55b5458f3de9e4819a61fc1
Ashish Singla <ashish.singla@polestarllp.com>
2016-09-08 20:18:43 +0530

    a

--- public/output/js/commonFunc.js
+++ public/output/js/commonFunc.js
@@ -16,6 +16,14 @@ function messageBox(message) {
 	$("#activeBugMasterMessageModal").modal("show");
 }
 // end messageBox
+// add id on page where you want to use this function eg. <div id="ModalAddMesage"></div>
+function messageBox2(message,value) {
+	var msg=message;
+	var messageBox ='<div class="modal fade" id="activeBugMasterMessageModal" role="dialog" data-backdrop="static" data-keyboard="false"><div class="modal-dialog"><div class="modal-content" id="activeBugMasterMsgBox"> <div class="modal-header text-center" style="border-top:0px;"> <span>Message</span> </div><div class="modal-body text-center"> <span id="cMessage1">'+msg+'</span> </div><div class="modal-footer text-center" style="border-top:0px;"> <button id="activeBugMasterCloseMessage" type="submit" data-dismiss="modal" class="btn btn-default" style="background-color:#f05f40;color:white;padding-top: 4px; width:100px;height:30px;margin-top:10px;" onclick='+value1+';>Close</button></div></div></div></div>';
+	$("#ModalAddMesage").html(messageBox);
+	$("#activeBugMasterMessageModal").modal("show");
+}
+// end messageBox
 // add id on page where you want to use this function eg. <div id="ModalAdd"></div>
 function confirmBox2(value,name,flag) { 
 	var msg="";
@@ -31,7 +39,11 @@ function confirmBox2(value,name,flag) {
 		msg +='Mail will be send to for activation.Do you want to continue?';
 	} else if(flag == 7) {
 		msg +='Do you want to create WBS?';
-	} 
+	} else if(flag == 8) {
+		msg +='Do you want to make this project inactive?';
+	} else if(flag == 9) {
+		msg +='Do you want to make this project active?';
+	}  
 	var confirmBox ='<div class="modal fade" id="activeBugMasterModal" role="dialog" data-backdrop="static" data-keyboard="false"><div class="modal-dialog" id="commonModalId"><div class="modal-content" id="activeBugMasterCnfrmMsgBox"> <div class="modal-header text-center" style="border-top:0px;"> <span>Message</span> </div><div class="modal-body text-center"> <span id="cMessage1">'+msg+'</span> </div><div class="modal-footer text-center" style="border-top:0px;"> <button id="activeBugMasterCloseMessage" type="reset" data-dismiss="modal" class="btn btn-default" style="background-color:#f05f40;color:white;padding-top: 4px; width:100px;height:30px;margin-top:10px;">Cancel</button> <button id="activeBugMasterActive" type="submit" data-dismiss="modal" class="btn btn-default" style="background-color:#f05f40;color:white;padding-top: 4px; width:100px;height:30px;margin-top:10px;" onclick='+name+'('+value+');>Ok</button> </div></div></div></div>';
 	$("#ModalAdd").html(confirmBox);
 	$("#activeBugMasterModal").modal("show");
