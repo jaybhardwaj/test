e8e877dad04d3de98d3eedba089b0b631341869f
amit_polestar <amit.kumar1@polestarllp.com>
2016-12-05 11:07:40 +0530

    changes

--- app.js
+++ app.js
@@ -29,9 +29,9 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(session({
   // store: new RedisStore({host: 'localhost', port: 6379}),
-  secret: 'a234l7678s746kmdtjyjuyfjt2tyj4fhn5fgh24k7lu6jj4j75k2j56lkh24lk4j2l'
-  // resave: false,
-  // saveUninitialized: true
+  secret: 'a234l7678s746kmdtjyjuyfjt2tyj4fhn5fgh24k7lu6jj4j75k2j56lkh24lk4j2l',
+  resave: false,
+  saveUninitialized: true
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 var env = process.env.NODE_ENV = (process.env.NODE_ENV || 'development');
