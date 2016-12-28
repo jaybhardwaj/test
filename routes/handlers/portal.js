52751500a595e66cec73a72ca43aab4f90a8817d
swapnil405 <guptaswapnil405@gmail.com>
2016-12-28 12:23:10 +0530

    table-responsive

--- routes/handlers/portal.js
+++ routes/handlers/portal.js
@@ -616,7 +616,7 @@ FilterDataForSelect:function(req,res,next){
      },
 
  submitAssignmenttree: function(req, res, next) {
-
+    //console.log("req.body-----",req.body);
            modelPortal.submitAssignment(
             req.body.flag,
             req.body.assignmentId,
@@ -2029,7 +2029,7 @@ modelPortal.projectfortree(req.session.retailerId,req.body.flag ,function(err,re
                 ////console.log("there is an error",err);
             }   
             else{
-               // console.log("project tree view",result);
+               // console.log("project****",result);
                  res.json(result);
                 } 
         });
@@ -2042,7 +2042,7 @@ modelPortal.showclients(req.body.flag,req.session.retailerId ,function(err,resul
                 ////console.log("there is an error",err);
             }   
             else{
-               // console.log("project tree view",result);
+             //  console.log("project tree view",result);
                  res.json(result);
                 } 
         });
@@ -2083,7 +2083,9 @@ modelPortal.activeinactive(req.body.clientid,req.body.actval,req.body.flag ,func
             }   
             else{
                // console.log("project tree view",result);
+              
                  res.json(result);
+                // console.log("myresult******",result);
                 } 
         });
    },
@@ -5671,9 +5673,14 @@ upload_resume:function(req,res,next){
             if(err){
             }   
             else{
+               if(result != ''){
                 req.body.emailId=result[0][0].userEmail;
                 req.body.firstName=result[0][0].firstName;
+
+               // console.log(result[0][0].userEmail,"email");
+               // console.log(result[0][0].firstName,"firstName");
                    next();
+                }
                 }  
         });
     },
@@ -5683,7 +5690,7 @@ upload_resume:function(req,res,next){
            mailTemplates.retailerRegistration(req.body.firstName, req.body.emailId,req.body.randomPassword,function(error, result) {
                          if (error) {
                          }
-
+                //  console.log("mailresult",result);
                          res.json(result);
                          
                      });
